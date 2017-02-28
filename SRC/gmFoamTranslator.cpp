4d13bdd410999ee4c9502b5c1ff0f9b4428f7d9c
hexiang6666 <hexwang@ucdavis.edu>
2017-02-27 20:54:25 -0800

    add some file headers

--- SRC/gmFoamTranslator.cpp
+++ SRC/gmFoamTranslator.cpp
@@ -1,4 +1,3 @@
-
 /********************************************************************************************************
 * File:              gmFoamTranslator.cpp                                              
 * Author:            Hexiang                                     | Boris Jeremic,                       *
@@ -7,7 +6,7 @@
 * Rev:               Version 1                                   | jeremic@ucdavis.edu                  *
 * Email:             hexwang@ucdavis.edu                         | Computational Geomechanics Group     *
 * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * 
-*                           Last Modified time: 2017-02-04 20:27:34                                     *            
+*                           Last Modified time: 2017-02-27 20:46:55                                     *            
 *  * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *         
 * The copyright to the computer program(s) herein is the property of Hexiang Wang and Boris Jeremic     *
 * The program(s) may be used and/or copied only with written permission of Hexiang Wang or in accordance* 
--- SRC/gmfoam_namespace.h
+++ SRC/gmfoam_namespace.h
@@ -1,3 +1,18 @@
+/********************************************************************************************************
+* File:              gmfoam_namespace.h                                              
+* Author:            hexiang6666                                 | Boris Jeremic,                       *
+* Date:              2017-02-26 20:45:17                         | University of California, Davis,95616*
+* Description:       #############                               | California                           *
+* Rev:               Version 1                                   | jeremic@ucdavis.edu                  *
+* Email:             hexwang@ucdavis.edu                         | Computational Geomechanics Group     *
+* * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * 
+*                           Last Modified time: 2017-02-27 20:43:28                                     *            
+*  * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *         
+* The copyright to the computer program(s) herein is the property of Hexiang Wang and Boris Jeremic     *
+* The program(s) may be used and/or copied only with written permission of Hexiang Wang or in accordance* 
+* with the terms and conditions stipulated in the agreement/contract under which the program have been  *
+* supplied.                                                                                             *
+*********************************************************************************************************/
 #ifndef GMFOAM_NAMESPACE_H
 #define GMFOAM_NAMESPACE_H
 
